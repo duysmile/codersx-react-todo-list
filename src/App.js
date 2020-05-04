37ec23da3fc17913ccb12b5f6a614d9a96117b70
Duy Nguyen <duy.nguyen@gonjoy.com>
2020-05-04 16:20:21 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <ul>
+        <li>Hello world</li>
+        <li>Hello everyone</li>
+        <li>Hello CodersX</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
